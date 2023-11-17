16f8702115aea97ae9d51d82fdef53efba068c29
lawrencea13 <105841326+lawrencea13@users.noreply.github.com>
2023-11-16 22:54:01 -0500

    added files for version 0.0.2
    upload what is essentially an alpha version of this mod

---
+++ Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,22 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCTutorialMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void infiniteSprintPatch(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCModSync;
+using LCTutorialMod.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCTutorialMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class TutorialModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Poseidon.LCTutorialMod";
+        private const string modName = "LC Tutorial Mod";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static TutorialModBase Instance;
+
+        internal ManualLogSource mls;
+
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The test mod has awaken :)");
+
+            harmony.PatchAll(typeof(TutorialModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+            mls = Logger;
+        }
+
+        public void modURL(ModSyncPlugin sender)
+        {
+            
+            sender.getModURLandName("https://github.com/lawrencea13/LoadObjectExample/releases/download/TestForModSync/LCTutorialMod.dll", "LCTutorialMod.dll");
+
+        }
+
+
+    }
+}
